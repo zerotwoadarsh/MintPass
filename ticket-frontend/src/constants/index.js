d70b246a4d221747ce4b631aea1c58fcc14ddff4
Adarsh Kushwaha <zerotwoadarsh@gmail.com>
2025-08-19 05:54:45 +0530

    completing the frontend

---
+++ ticket-frontend/src/constants/index.js
@@ -0,0 +1,7 @@
+import contractABI from './TicketMaster.json';
+
+// You need to replace this placeholder with your actual address
+export const contractAddress = "0x6a7E0976312444f497bC2A5ebA899Be48A61136A";
+
+// This line reads the ABI from the other file
+export const abi = contractABI.abi;
